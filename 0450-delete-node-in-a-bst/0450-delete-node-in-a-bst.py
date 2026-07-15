b787337aaee4e4fa22ac2ff81cc3e75c1c2d743e
Keerthireddy-Bachamola <bachamolakeerthireddy@gmail.com>
2026-07-15 18:54:37 +0530

    Time: 0 ms (100%), Space: 22.1 MB (42.29%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -5,35 +5,31 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        def getmin(r):
+    def getmin(self,r):
             if r is None:
                 return None
             while(r.left):
                 r=r.left
             return r
-        def fun(root,key):
-            if root==None:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if root==None:
                 return None
-            if key<root.val:
-                root.left=fun(root.left, key)
-            if key>root.val:
-                root.right=fun(root.right, key)
-            if key==root.val:
-                if root.left is None and root.right is None:
-                    return None
-                if root.left is not None and root.right is None:
-                    return root.left
-                if root.left is  None and root.right is not None:
-                    return root.right
-                else:
-                    temp=getmin(root.right)
-                    root.val=temp.val
-                    root.right=fun(root.right,temp.val)
-            return root
-
-        return fun(root, key)
-
-             
+        if key<root.val:
+            root.left=self.deleteNode(root.left, key)
+        if key>root.val:
+            root.right=self.deleteNode(root.right, key)
+        if key==root.val:
+            if root.left is None and root.right is None:
+                return None
+            if root.left is not None and root.right is None:
+                return root.left
+            if root.left is  None and root.right is not None:
+                return root.right
+            else:
+                temp=self.getmin(root.right)
+                root.val=temp.val
+                root.right=self.deleteNode(root.right,temp.val)
+        return root
+        
             
         
